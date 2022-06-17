07907cf672cc5b4c91fecee4ba61e8013e5da0ff
Berend Bleeker <b.f.bleeker@student.tudelft.nl>
2022-06-17 23:44:56 +0200

    Jobs created which can be used for the schedulers, this includes updating mentions and timelines and replying to those tweets

--- check.py
+++ check.py
@@ -1,18 +1,35 @@
 import db
-
-
-# Store in db function
-def store(response, username, user_id):
-    """
-    Function that process the tweet response to store it in a db
-    :param response: Response object gotten with a GET request
-    :param username: Username from the tweets owner
-    :param user_id: User id from the tweets owner
-    """
-    data = response[0]
-    for tweet in data:
-        if db.check_tweet_id(tweet["id"]) == 0:
-            db.add_tweet(tweet["text"], tweet["id"], user_id, username, False)
+import filter
+import openai3
+import twitter
+import setup
 
 
 # Check functions
+def update():  # TODO: put update timelines and mentions in one function
+    twitter.update_mentions()
+    twitter.update_timelines(users=setup.get_target_users())
+
+
+# Post functions
+def reply():
+    tweet = db.get_tweet_for_response()
+
+    if tweet is not None:
+        # Generate a response
+        text = filter.filter_all(tweet["text"])
+        response = openai3.response(prompt=text)
+
+        # Insert generated response in table
+        db.insert_response(response, "openai3")
+
+        # Respond to the tweet & update the Tweets table
+        twitter.reply_to_with(tweet["tweet_id"], response)
+        db.update_response(1, tweet["tweet_id"])
+
+    else:
+        pass
+
+
+def post(): # TODO: generate random tweets
+    print("Random generated tweet")
