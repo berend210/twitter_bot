6d80a46e750c5742a2da7c7ffb95ca90def83565
Berend Bleeker <b.f.bleeker@student.tudelft.nl>
2022-06-17 23:44:17 +0200

    Adds more functionality to the functions we use to get data from the twitter API and make the bot post messages.

--- twitter.py
+++ twitter.py
@@ -1,48 +1,63 @@
-import main
+import db
+import setup
 
-api = main.client
-
-def update_timelines(start_time = None, do_print = False):
-	"""
-	COUNTS TOWARDS MONTHLY CAP!!
-	Gets a timeline update (latest 10 tweets) from all the targeted users
-	optional: print all the results
-	"""
-	targets = main.target_ids_list
-	responses = []
-
-	if (start_time != None):
-		pass
-		# TODO: api call that inludes start_time (uses Oauth2.0)
-		# for target in targets:
-		# 	responses.append(api.get_users_tweets(start_time=s start_time target))
-
-	for target in targets:
-		responses.append(api.get_users_tweets(target))
-
-	if do_print:
-		for response in responses:
-			print(response)
-
-	return responses
+api = setup.get_client()
 
 
-def build_tweet(input, is_retarded = False):
-	text = input
-	if is_retarded:
-		text = ret_speech(text)
-	response = post_tweet(text)
-	print(response)
+def get_username(id):
+    return api.get_user(id=id)[0]['username']
 
+
+def update_timelines(users, do_print=False):
+    """
+    COUNTS TOWARDS MONTHLY CAP!!
+    Gets a timeline update (latest 10 tweets) from all the targeted users
+    optional: print all the results
+    :param users: a list of user id's
+    """
+    targets = get_user_ids(users)
+    responses = []
+
+    for target in targets:
+        since = db.get_last_id(target)
+        db.store(api.get_users_tweets(target, since_id=since), get_username(target), target)
+
+    if do_print:
+        for response in responses:
+            print(response)
+
+    return responses
+
+
+def update_mentions(do_print=False):
+    id = 1515353783567634446
+    response = api.get_users_mentions(id=id, since_id=db.get_last_id(), max_results=10)
+    db.store(response=response)
+
+    if do_print is True and response[0] is not None:
+        print(response)
+        for tweet in response[0]:
+            print(tweet)
+
+    return response
+
+
+def build_tweet(input, is_retarded=False):
+    text = input
+    if is_retarded:
+        text = ret_speech(text)
+    response = post_tweet(text)
+    print(response)
 
 
 def reply_to_with(tweet_id, text):
-	"""
-	Replies to comment tweet_id with reply reply
-	:param tweet_id: comment to reply on
-	:param text: the reply that will be posted
-	"""
-	api.create_tweet(text=text, in_reply_to_tweet_id=tweet_id)
+    """
+    Replies to comment tweet_id with reply reply
+    :param tweet_id: comment to reply on
+    :param text: the reply that will be posted
+    """
+    api.create_tweet(text=text, in_reply_to_tweet_id=tweet_id)
+
 
 def post_tweet(tweet):
     """
@@ -51,6 +66,19 @@ def post_tweet(tweet):
     :return: status of creation of the tweet
     """
     return api.create_tweet(text=tweet)
+
+
+def get_user_ids(list):
+    """
+    Sets all the ids for the users,
+    so we only have to input usernames
+    """
+    target_ids = []
+    for target in list:
+        user = api.get_user(username=target)
+        target_ids.append(user.data.id)
+
+    return target_ids
 
 
 def ret_speech(original_text):
@@ -73,3 +101,4 @@ def ret_speech(original_text):
         result_text += c
 
     return result_text
+
