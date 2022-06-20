af5ba425173a81029887dc76586cd9193dda82f8
Berend Bleeker <b.f.bleeker@student.tudelft.nl>
2022-06-20 10:41:44 +0200

    Build in the translator functionality together with a more complex system for generating messages.

--- openai3.py
+++ openai3.py
@@ -1,11 +1,17 @@
 import openai
 import setup
+import translator
 
 # Load your API key from an environment variable or secret management service
 openai.api_key = setup.get_openai()
 
 
-def response(prompt=None, tokens=10, engine=3):
+def prompt_builder(text):
+    prompt = "Tweet: " + text + "\n" + "You: "
+    return prompt
+
+
+def response(prompt=None, tokens=20, engine=3, translate_nl=True, build_prompt=False):
     """
     Function which gets a gpt-3 completion response by the OpenAI API
     :param prompt: The text which should be used for a response
@@ -13,16 +19,42 @@ def response(prompt=None, tokens=10, engine=3):
     :param engine: Which engine to use
     :return:
     """
+    tran = 0
+    gentex = 0
+    engine_type = ["text-davinci-002", "text-curie-001", "text-babbage-001", "text-ada-001"]
+
     if prompt is None:
         return "Try again please"
 
-    engine_type = ["text-davinci-002", "text-curie-001", "text-babbage-001", "text-ada-001"]
-    # Temperature is always 1 as we want the api to take as much risk as possible
-    resp = openai.Completion.create(engine=engine_type[engine], prompt=prompt, temperature=1, max_tokens=tokens)
+    if translate_nl:
+        tran = translator.translate(prompt, "nl", "en")
+    else:
+        tran = prompt
+
+    if build_prompt:
+        gentex = prompt_builder(tran)
+    else:
+        gentex = tran
+
+    try:
+        resp = openai.Completion.create(engine=engine_type[engine], prompt=gentex, temperature=1, max_tokens=tokens)
+    except:
+        return None
+
     # Returns only the text produced by the API
-    return resp["choices"][0]["text"]
+    text = resp["choices"][0]["text"]
+    print("EN: " + text)
+
+    if translate_nl:
+        end = translator.translate(text.strip(), "en", "nl")
+        print("NL: " + end + "\n\n")
+
+    return end
 
 
 def test_openai3():
-    out = response(prompt="Motivational quote", tokens=30)
+    out = response(prompt="Motiverende tekst", tokens=22, build_prompt=True)
     print(out)
+
+
+# test_openai3()
