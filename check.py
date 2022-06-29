e515cd87090581b6d791092586b265ecb746027d
Berend Bleeker <b.f.bleeker@student.tudelft.nl>
2022-06-29 21:56:46 +0200

    Added documentation and perfected the replying on Tweets

--- check.py
+++ check.py
@@ -3,11 +3,15 @@ import filter
 import openai3
 import twitter
 import setup
+import random
 
 
 # Check functions
 def update():
-    # TODO: fix that we don't only get mentions but replies as well
+    """
+    Job which updates all the tweets the bot checks on.
+    :return: None
+    """
     last_id = db.get_last_id()
     twitter.update_mentions(last_id=last_id)
     twitter.update_replies(last_id=last_id)
@@ -16,12 +20,21 @@ def update():
 
 # Post functions
 def reply():
-    # TODO: use an translator for the replies (EN -> NL)
+    """
+    Replies to all the non-replied Tweets in the database.
+    :return: None
+    """
     tweet = db.get_tweet_for_response()
 
     if tweet is not None:
         tweet_id = tweet[0]
         tweet_text = tweet[1]
+        tweet_acc = tweet[2]
+
+        # Has a chance of replying on someone's tweet
+        if tweet_acc is not None and random.randint(0, 10) > 5:
+            db.update_response(1, tweet_id)
+            return
 
         # Generate a response
         text = filter.filter_all(tweet_text)
@@ -37,9 +50,9 @@ def reply():
         twitter.reply_to_with(tweet_id, response)
         db.update_response(1, tweet_id)
 
-    else:
-        pass
-
 
 def post():  # TODO: generate random tweets
+    # Let the openai resond to messages we created
+    # Fetch the openai random tweets from Twitter and use that to make posts
+    # Send random words to openai to invoke some reaction
     print("Random generated tweet")
