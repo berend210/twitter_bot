24f94fd4b1cbfafd4e4510e7a878d5ffa0a59472
Berend Bleeker <b.f.bleeker@student.tudelft.nl>
2022-06-29 21:59:04 +0200

    Added proper documentation and removed AVG sensitive parts from the code

--- openai3.py
+++ openai3.py
@@ -1,60 +1,80 @@
 import openai
 import setup
 import translator
+import random
+import twitter
+from difflib import SequenceMatcher
 
 # Load your API key from an environment variable or secret management service
 openai.api_key = setup.get_openai()
 
 
 def prompt_builder(text):
-    prompt = "Tweet: " + text + "\n" + "You: "
+    """
+    Prompt builder for the openai API. The prompt text is in the setup.py.
+    :param text: string used for the prompt.
+    :return: Prompt with the wanted text.
+    """
+    prompt_list = setup.get_prompts()
+    chance = random.randint(0, len(prompt_list) - 1)
+    prompt = prompt_list[chance][0].replace("#text#", text)
+    print(prompt_list[chance][1])
     return prompt
 
 
-def response(prompt=None, tokens=20, engine=3, translate_nl=True, build_prompt=False):
+# TODO: Zinnen checken of ze lijken op de inp en dan Retard speech toevoegen
+# TODO: Taalfilter bouwen
+def response(prompt=None, tokens=25, engine=3, translate_nl=True, build_prompt=True):
     """
-    Function which gets a gpt-3 completion response by the OpenAI API
-    :param prompt: The text which should be used for a response
-    :param tokens: Max amount of tokens to be used
-    :param engine: Which engine to use
+    Function which gets a gpt-3 completion response by the OpenAI API.
+    :param prompt: The text which should be used for a response.
+    :param tokens: Max amount of tokens to be used.
+    :param engine: Which engine to use.
+    :param translate_nl: Whether it should to translated to Dutch or not.
+    :param build_prompt: Whether the prompt builder should be used or not.
     :return:
     """
-    tran = 0
-    gentex = 0
+
     engine_type = ["text-davinci-002", "text-curie-001", "text-babbage-001", "text-ada-001"]
+    inp = prompt
 
     if prompt is None:
         return "Try again please"
 
     if translate_nl:
-        tran = translator.translate(prompt, "nl", "en")
-    else:
-        tran = prompt
+        prompt = translator.translate(prompt, "auto", "en")
+        print("First translation: " + prompt)
 
     if build_prompt:
-        gentex = prompt_builder(tran)
-    else:
-        gentex = tran
+        prompt = prompt_builder(prompt)
 
     try:
-        resp = openai.Completion.create(engine=engine_type[engine], prompt=gentex, temperature=1, max_tokens=tokens)
+        resp = openai.Completion.create(engine=engine_type[engine], prompt=prompt, temperature=1, max_tokens=tokens)
     except:
         return None
 
     # Returns only the text produced by the API
     text = resp["choices"][0]["text"]
-    print("EN: " + text)
+    print("OPENAI: " + text)
 
     if translate_nl:
-        end = translator.translate(text.strip(), "en", "nl")
-        print("NL: " + end + "\n\n")
+        text = translator.translate(text.strip(), "en", "nl")
 
-    return end
+    # Makes the output weird when the inp is too similar.
+    if SequenceMatcher(None, inp, text).ratio() > 0.7:
+        text = twitter.ret_speech(text)
+
+    print("Final resp: " + text)
+    return text
 
 
 def test_openai3():
-    out = response(prompt="Motiverende tekst", tokens=22, build_prompt=True)
-    print(out)
+    """
+    OpenAI test function.
+    :return: None.
+    """
+    out = response(prompt="Beren", tokens=22, build_prompt=True)
+    print("Test openAI API: " + out)
 
 
-# test_openai3()
+test_openai3()
